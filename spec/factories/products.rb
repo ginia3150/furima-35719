1121b514e299fed8c55555abae255b8ee91a649e
ginia3150 <ginia3150@gmail.com>
2021-06-01 18:08:38 +0900

    修正2

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :product do
     name {Faker::Name.last_name}
     description {"test"}
-    category_id {"3"}
-    condition_id {"3"}
-    shipping_charge_id {"3"}
-    shipping_area_id {"3"}
-    day_to_ship_id {"3"}
+    category_id {3}
+    condition_id {3}
+    shipping_charge_id {3}
+    shipping_area_id {3}
+    day_to_ship_id {3}
     price {"456789"}
     association :user
 
